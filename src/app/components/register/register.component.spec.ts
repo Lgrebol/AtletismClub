1a6bff2b85e0cbee3819ca1a16cf9e28e85e668a
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-09-24 19:50:39 +0200

    Title h1 implemented

---
+++ src/app/components/register/register.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterComponent } from './register.component';
+
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+  let compiled: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegisterComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled=fixture.nativeElement as HTMLElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have the title Inscripció Proves Atletisme', () => {
+    expect(compiled.querySelector('h1')?.textContent).toBe('Inscripció Proves Atletisme');
+});
+});
